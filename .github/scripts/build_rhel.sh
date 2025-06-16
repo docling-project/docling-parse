ae09944feded4899524af45c750bfdacd4a9522e
rmdg88 <rmdg88@gmail.com>
2025-06-16 14:35:07 +0100

    switch from poetry to uv

--- .github/scripts/build_rhel.sh
+++ .github/scripts/build_rhel.sh
@@ -41,9 +41,7 @@ sudo -E XDG_RUNTIME_DIR= podman build --progress=plain \
             /src/docling_parse*.tar.gz \
         && pip3.11 install /dist/docling_parse*.whl \
         && python3.11 -c 'from docling_parse.pdf_parsers import pdf_parser_v1, pdf_parser_v2'
-    
-    RUN uv pip install -e .
-    
+
     COPY ./tests /src/tests
 
     RUN pip3.11 install pytest \
