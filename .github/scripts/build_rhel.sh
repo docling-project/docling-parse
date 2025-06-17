9d8beecec942ebbeed8e16cb1111319424c7e259
rmdg88 <rmdg88@gmail.com>
2025-06-17 16:09:29 +0100

    switch from poetry to uv

--- .github/scripts/build_rhel.sh
+++ .github/scripts/build_rhel.sh
@@ -3,6 +3,9 @@
 set -e  # trigger failure on error - do not remove!
 set -x  # display command on output
 
+# Build the Python package with uv
+uv build
+
 sudo -E XDG_RUNTIME_DIR= podman build --progress=plain \
              --build-arg USE_SYSTEM_DEPS="$USE_SYSTEM_DEPS" \
              -f - . <<EOF
@@ -21,25 +24,25 @@ sudo -E XDG_RUNTIME_DIR= podman build --progress=plain \
             autoconf automake binutils cmake gcc gcc-c++ git glibc-devel glibc-headers glibc-static kernel-devel libtool libstdc++-devel make ninja-build pkgconfig zlib-devel \
             python3.11 python3.11-pip python3.11-devel \
             libjpeg-turbo-devel libpng-devel qpdf-devel json-devel utf8cpp-devel loguru-devel cxxopts-devel \
-            uv pybind11 wheel \
         && dnf clean all
 
     WORKDIR /src
 
-    COPY ./ /src
-
-    RUN uv build
+    COPY ./dist/*.tar.gz .
 
     ENV USE_SYSTEM_DEPS=on
 
     # pre-install build requirements + wheel for "--no-build-isolation"
     # build docling-parse wheel in an isolated network namespace (unshare -rn)
     # install the wheel and its dependencies
-    RUN unshare -rn pip3.11 wheel \
+    RUN pip3.11 install uv pybind11 wheel \
+        && unshare -rn pip3.11 wheel \
             --no-deps --no-build-isolation -w /dist/ \
             /src/docling_parse*.tar.gz \
         && pip3.11 install /dist/docling_parse*.whl \
         && python3.11 -c 'from docling_parse.pdf_parsers import pdf_parser_v1, pdf_parser_v2'
+    
+    COPY ./tests /src/tests
 
     RUN pip3.11 install pytest \
         && pytest
--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
-from setuptools import setup
-from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools import setup, Distribution
+from setuptools.command.build_py import build_py as _build_py
 import subprocess
 import sys
-import os
 
-class CustomBuildExt(_build_ext):
+
+class CustomBuildPy(_build_py):
     def run(self):
         subprocess.check_call([sys.executable, "build.py"])
         super().run()
 
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
+
+
 setup(
-    cmdclass={"build_ext": CustomBuildExt},
+    name="docling-parse",
+    cmdclass={"build_py": CustomBuildPy},
+    distclass=BinaryDistribution,
+    zip_safe=False,
+    packages=["docling_parse"],
+    package_data={
+        "docling_parse": [
+            "*.so", "*.pyd", "*.dll",
+            "pdf_resources/*",
+            "pdf_resources_v2/*",
+        ]
+    },
+    include_package_data=True,
 )
