c811fceb84bc07a55440daf61269676900046b54
rmdg88 <rmdg88@gmail.com>
2025-06-18 13:29:30 +0100

    switch from poetry to uv

--- .github/scripts/build_rhel.sh
+++ .github/scripts/build_rhel.sh
@@ -35,7 +35,8 @@ sudo -E XDG_RUNTIME_DIR= podman build --progress=plain \
     # pre-install build requirements + wheel for "--no-build-isolation"
     # build docling-parse wheel in an isolated network namespace (unshare -rn)
     # install the wheel and its dependencies
-    RUN pip3.11 install setuptools pybind11 wheel \
+    RUN pip3.11 install --upgrade pip \
+        && pip3.11 install --upgrade "setuptools>=77.0.3" "wheel>=0.43.0,<1.0.0" "pybind11>=2.13.6" \
         && unshare -rn pip3.11 wheel \
             --no-deps --no-build-isolation -w /dist/ \
             /src/docling_parse*.tar.gz \
