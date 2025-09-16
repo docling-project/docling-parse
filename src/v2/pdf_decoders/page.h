9ad52a807bb037ae27243533c5877831aa7371d9
Peter Staar <taa@zurich.ibm.com>
2025-09-16 13:57:08 +0200

    updated the sanitization
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- src/v2/pdf_decoders/page.h
+++ src/v2/pdf_decoders/page.h
@@ -263,8 +263,10 @@ namespace pdflib
 
       {
 	//utils::timer _;
-	//sanitator.remove_adjacent_chars(page_cells, 0.5);
-	sanitator.remove_adjacent_cells(page_cells, 0.5);
+
+	//sanitator.remove_adjacent_cells(page_cells, 0.5);
+	sanitator.remove_duplicate_cells(page_cells, 0.5, true);
+
 	//std::cout << "pdf_sanitator<PAGE_CELLS>::remove_duplicate_chars " << _.get_time() << "\n";
       }
       
