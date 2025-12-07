f099a24117b44af1c27cfe604fa71ec0c7fb2137
Tim Black <timblaktu@gmail.com>
2025-12-07 13:52:24 -0800

    fix: apply nlohmann::json::boolean_t wrapper to all bool conversions
    - Fix push_back(bool) calls in page_cell.h for widget and left_to_right
    - Fix json assignment from bool in pdf_sanitators/cells.h
    - Resolves all C++20 template resolution issues with nlohmann_json 3.12

--- src/v2/pdf_sanitators/cells.h
+++ src/v2/pdf_sanitators/cells.h
@@ -123,8 +123,8 @@ namespace pdflib
 
 	  item["rendering_mode"] = cell.rendering_mode;
 
-	  item["widget"] = cell.widget;
-	  item["left_to_right"] = cell.left_to_right;
+	  item["widget"] = nlohmann::json::boolean_t(cell.widget);
+	  item["left_to_right"] = nlohmann::json::boolean_t(cell.left_to_right);
 	}
 
 	result.push_back(item);
