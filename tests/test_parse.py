bae37c1a8e1ec41dfab916a42e2b819130f356be
Peter Staar <taa@zurich.ibm.com>
2026-01-12 18:22:10 +0100

    fixing the regression tests (2)
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -18,7 +18,7 @@ from pydantic import TypeAdapter
 
 from docling_parse.pdf_parser import DoclingPdfParser, PdfDocument
 
-GENERATE = True
+GENERATE = False
 
 GROUNDTRUTH_FOLDER = "tests/data/groundtruth/"
 REGRESSION_FOLDER = "tests/data/regression/*.pdf"
