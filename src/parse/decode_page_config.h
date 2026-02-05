915d88d45593703739e4e399a6d7bd5cf5057e9c
Peter Staar <taa@zurich.ibm.com>
2026-02-05 11:21:36 +0100

    adding decode_page_config file
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

---
+++ src/parse/decode_page_config.h
@@ -0,0 +1,25 @@
+//-*-C++-*-
+
+#ifndef PDF_DECODE_PAGE_CONFIG_H
+#define PDF_DECODE_PAGE_CONFIG_H
+
+namespace pdflib
+{
+
+  struct decode_page_config
+  {
+    std::string page_boundary = "crop_box";
+
+    bool do_sanitization = true;
+
+    bool keep_char_cells = true;
+    bool keep_lines = true;
+    bool keep_bitmaps = true;
+
+    int max_num_lines = -1;   // -1 means no cap
+    int max_num_bitmaps = -1; // -1 means no cap
+  };
+
+}
+
+#endif
