d2e19f7c86621de202c336275e857b04f65ad321
Peter Staar <taa@zurich.ibm.com>
2026-07-10 07:02:47 +0200

    fixed the tests data
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

---
+++ tests/constants.py
@@ -0,0 +1,13 @@
+HF_DATASET_REPO_ID = "docling-project/regression-dataset-for-docling-parse"
+HF_DATASET_REVISION = "17980e180c394d7941cc6b47860a7d24570fceaf"
+
+PARSER_PAGE_RESTRICTIONS = {
+    "deep-mediabox-inheritance.pdf": [2],
+    "font_06.pdf": [1],
+    "font_07.pdf": [1],
+    "font_08.pdf": [1],
+    "font_09.pdf": [1],
+    "font_10.pdf": [1],
+    "font_11.pdf": [1, 2],
+    "2508.13113v2.pdf": [2, 9, 17],
+}
